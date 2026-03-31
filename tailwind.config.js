a1fdb6c9af6da3dc48b9ec03a523732350ff9549
lecristina <cristinale33@hotmail.com>
2026-03-31 14:38:01 -0300

    crie seu site feito o ladinding page

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Space Grotesk', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#8f00cc',
+          light: '#b44ddb',
+          dark: '#6b00a3',
+          50: '#f5e6ff',
+          100: '#e6c2ff',
+          200: '#cc80ff',
+          300: '#b34dff',
+          400: '#9f1aff',
+          500: '#8f00cc',
+          600: '#7300a6',
+          700: '#580080',
+          800: '#3d005a',
+          900: '#220033',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
